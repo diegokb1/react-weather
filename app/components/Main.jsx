f987b9805a7c3cbf9dd762470011f14d421119df
Diego Klappenbach <dklappenbach@vairix.com>
2020-07-03 09:35:02 -0300

    Test pipeline

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,12 +1,11 @@
 const React = require('react');
 const Nav = require('Nav');
-const Weather = require('Weather');
 
 const Main = ({ children }) => {
   return (
     <div>
       <Nav />
-      <h2>Main Component</h2>
+      <h2>Main Component!</h2>
       {children}
     </div>
   );
